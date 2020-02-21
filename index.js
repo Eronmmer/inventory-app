79a9cf7331437684bd17ac0d8ae43ca022c529ab
Erons <eronmmer@gmail.com>
2020-02-21 20:00:48 +0100

    this should be it

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static(path.resolve(__dirname, "client", "build")));
 
-  app.get("/", (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
