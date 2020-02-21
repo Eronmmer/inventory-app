228d6efcd73552b7620ca603c89f93f482b76646
Erons <eronmmer@gmail.com>
2020-02-21 23:51:53 +0100

    :)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const logger = require("./middleware/logger");
 const connectDb = require("./config/db");
